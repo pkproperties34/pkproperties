8e1dd92ff10cf3a9ab5b5fa1d26baa8bd3976884
saiprasanth03 <ssaiprasanth333@gmail.com>
2026-09-22 21:09:27 +0530

    Update CORS origin configuration in server

--- server/index.js
+++ server/index.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: [
+    "https://pkproperties.in",
+    "https://www.pkproperties.in"
+  ],
   credentials: true
 }));
 app.use(express.json());
